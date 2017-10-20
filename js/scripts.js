304912ff6c3d8d6b688319d14a65a3c1be43c052
Chris Quattrocchi <chrisquattrocchi@gmail.com>
2017-10-20 14:46:06 -0700

    basic logic structure in place, needs tweaking and styling

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,26 @@
 $(document).ready(function(){
   $("#trackform").submit(function(event){
     event.preventDefault();
-    var q1 = $("select#question1").val();
-    var q2 = $("select#question2").val();
-    var q3 = $("select#question3").val();
-    var q4 = $("select#question4").val();
-    var q5 = $("select#question5").val();
-    var q6 = $("select#question6").val();
-    alert(q1)
-    alert(q2)
-    alert(q3)
-    alert(q4)
-    alert(q5)
-    alert(q6)
+    //record variables
+    var q1 = parseInt($("select#question1").val());
+    var q2 = parseInt($("select#question2").val());
+    var q3 = parseInt($("select#question3").val());
+    var q4 = parseInt($("select#question4").val());
+    var q5 = parseInt($("select#question5").val());
+    var q6 = parseInt($("select#question6").val());
+    //logic
+    if (q1 === 2 && q3 === 2 && q5 === 2) {
+      $("#track-csharp").show();
+    } else if (q1 === 2 && q2 === 1 && q4 === 2) {
+      $("#track-java").show();
+    } else if (q2 === 2 && q4 === 2 && q5 === 2) {
+      $("#track-php").show();
+    } else if (q3 === 1 && q4 === 1 && q5 === 2) {
+      $("#track-ruby").show();
+    } else if (q2 === 2 && q4 === 1 && q5 === 2) {
+      $("#track-css").show();
+    } else {
+      $("#track-hunter").show();
+    }
   });//submit function
 });//doc ready function
